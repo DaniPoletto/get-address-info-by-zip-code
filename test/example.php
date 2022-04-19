24477c15e26b90b441255ebcb90229178eebb7ec
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-04-19 20:44:07 -0300

    separando codigo fonte e teste | separeting source and test

--- test/example.php
+++ test/example.php
@@ -2,8 +2,8 @@
 
 <head>
     <title></title>
-    <link href="css/sweetalert.css" rel="stylesheet">
-    <script src="js/sweetalert.min.js"></script>
+    <link href="../css/sweetalert.css" rel="stylesheet">
+    <script src="../js/sweetalert.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -47,6 +47,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
-<script src="js/jquery-3.6.0.min.js"></script>
-<script src="js/jquery.mask.js"></script>
-<script src="js/get-address-info-by-zip-code.js"></script>
+<script src="../js/jquery-3.6.0.min.js"></script>
+<script src="../js/jquery.mask.js"></script>
+<script src="../src/get-address-info-by-zip-code.js"></script>
